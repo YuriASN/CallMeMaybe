f7239576e9a8cf2a7bbf10786208b9970c0d2717
Yuri ASN <yuriasn@hotmail.com>
2026-08-27 21:00:12 +0100

    Time wrapper to see how long a function takes to run
    Simple time wrapper that returns the result + the time that took for the function to run.
    It removes then the abillity to do a augumented assign (+=) as it'll return a tuple.

---
+++ src/timeit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from time import time
+from functools import wraps
+from typing import Any
+from collections.abc import Callable
+
+
+def time_it(func: Callable) -> Callable:
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        start = time()
+        result = func(*args, **kwargs)
+        end = time()
+        run_time: float = round(end - start, 3)
+        return result, run_time
+
+    return wrapper
